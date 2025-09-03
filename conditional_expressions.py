2e0e0414fea2a0fb131d12b38286da0cc2734b45
TehamiKamdar <tehamikamdar15@gmail.com>
2025-09-03 17:41:22 +0500

    Conditional Statements Practice

---
+++ conditional_expressions.py
@@ -0,0 +1,13 @@
+# Age Checker Online
+# age = int(input("Enter Your Age: "))
+# if(age >= 18 and age < 65):
+#   print("You are eligible")
+# elif(age > 65):
+#   print("You are too old for this position")
+# else:
+#   print("You are not eligible")
+  
+# Practice Set for If Elif Else 
+string = input("Enter Keyword: ")
+if(string == "Make a lot of money" or string == "buy now" or string == "subscribe this" or string == "click this"):
+  print("The words you've used defines scammed")
