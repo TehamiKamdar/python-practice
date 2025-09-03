d7e80d2e5360ff93e1a12d6518922acfe6187814
TehamiKamdar <tehamikamdar15@gmail.com>
2025-09-03 16:25:58 +0500

    Sets Practice

---
+++ sets.py
@@ -0,0 +1,30 @@
+# emptySet = set();
+# filledSet = {1,2,3,4,5,6}
+
+# print(emptySet)
+# print(filledSet)
+
+# emptySet.add(1)
+# emptySet.add(2)
+# emptySet.add(3)
+# emptySet.add(4)
+
+# print(emptySet)
+
+# emptySet.remove(4)
+# print(emptySet)
+
+# Union and InterSections
+
+U = {1,2,3,4,5,6,7,8,9,10}
+A = {1,2,3,4,5}
+B = {6,7,8,9,10}
+C = {1,3,5,7,9}
+
+# A union B
+Ans1 = A.union(B)
+# print(Ans1)
+
+# A intersection B
+Ans2 = A.intersection(B)
+print(Ans2)
