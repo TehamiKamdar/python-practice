5bc4aa3a82a388cb8068299fb25d09351789331a
TehamiKamdar <tehamikamdar15@gmail.com>
2025-09-05 00:15:24 +0500

    Half Practice Done Python

---
+++ loops.py
@@ -0,0 +1,66 @@
+# for i in range(1, 12):
+#     print(i, "*", num, "=", i * num)
+
+# a = 1 * 4
+# print(a)
+
+# Q1. Write a program to print multiplication table of a given number using for loop. 
+# num = 12
+# for i in range(1,11):
+#     print(f"{i} X {num} = {num*i}")
+
+
+# 2. Write a program to greet all the person names stored in a list ‘l’ and which starts with S. 
+# l = ["Harry", "Soham", "Sachin", "Rahul"];
+# for i in l:
+#     if(i.startswith("S")):
+#         print(f"Hello {i}")
+
+# 3. Attempt problem 1 using while loop. 
+# num = 12
+# i = 1
+# while(i < 11):
+#     print(f"{num} X {i} = {num*i}")
+#     i+=1
+
+# 4. Write a program to find whether a given number is prime or not. 
+# num = int(input("Enter number: "))
+# for i in range(2, num):
+#     if(num%i) == 0:
+#         print("It is not a prime number")
+#         break
+# else:
+#     print("It is a prime number")
+
+# 5. Write a program to find the sum of first n natural numbers using while loop. 
+# num = int(input("Enter Number: "))
+# i = 1
+# sum = 0
+
+# while(i<=num):
+#     sum+=i
+#     i+=1
+# print(sum)
+
+# 6. Write a program to calculate the factorial of a given number using for loop. 
+
+
+# 7. Write a program to print the following star pattern. 
+#   * 
+#  *** 
+# ***** for n = 3 
+
+
+# 8. Write a program to print the following star pattern: 
+# * 
+# ** 
+# ***      for n = 3 
+
+
+# 9. Write a program to print the following star pattern. 
+# * * * 
+# *   *   for n = 3 
+# * * *  
+
+
+# 10. Write a program to print multiplication table of n using for loops in reversed order.
